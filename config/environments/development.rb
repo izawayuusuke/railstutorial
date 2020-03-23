0a90833b202340e8ecf295689619e8f283f832ae
izawayuusuke <yuu05020@gmail.com>
2020-03-23 04:33:40 +0000

    Add account activation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,9 +27,13 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :test
+  
+  host = 'a081c086a335456a83f0709ba78021e0.vfs.cloud9.ap-northeast-1.amazonaws.com'
+  
+  config.action_mailer.default_url_options = { host: host, protocol: 'https'}
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200321044624) do
+ActiveRecord::Schema.define(version: 20200322084538) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,10 @@ ActiveRecord::Schema.define(version: 20200321044624) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin", default: false
+    t.string "activation_digest"
+    t.boolean "activated", default: false
+    t.datetime "activated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
